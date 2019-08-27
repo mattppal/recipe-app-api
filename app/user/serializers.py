afd00b37a2b45141423ee3ccecd36c5460078ebb
Matt <mattppalmer@gmail.com>
2019-08-27 10:14:11 -0400

    added tests & implemented manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -17,6 +17,17 @@ class UserSerializer(serializers.ModelSerializer):
         """create a new user with encrypted password & return in"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """update a user, setting the password & return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """serializer for the user authenication object"""
