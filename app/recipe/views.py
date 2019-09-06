347bec8e4eb48497dc54f3839b41637d7e0827f6
Matt <mattppalmer@gmail.com>
2019-09-06 07:53:10 -0400

    finnished working ingredient api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -26,14 +26,19 @@ class TagViewSet(viewsets.GenericViewSet,
 
 
 class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin):
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
     '''manage ingredients in the database'''
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
-    serializer_class = serializers.TagSerializer
+    serializer_class = serializers.IngredientSerializer
 
     def get_queryset(self):
         '''return objs for the current authorized user only'''
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        '''create a new ingredient'''
+        serializer.save(user=self.request.user)
